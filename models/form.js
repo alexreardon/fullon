aae2e30bc03e1ae9cc2d4c0113c78633746bbbf4
Alex Reardon <alexreardon@gmail.com>
2013-07-24 06:12:51 +1000

    progress

--- models/form.js
+++ models/form.js
@@ -1,25 +1,12 @@
-var Backbone = require('backbone'),
-	_ = require('underscore'),
-	validation = require('backbone-validation');
-
-_.extend(Backbone.Model.prototype, validation.mixin);
-
-var FormModel = Backbone.Model.extend({
-	validation: {
-		name: {
-			required: true
-		},
-		age: {
-			range: [10, 80]
-		},
-		email: {
-			pattern: 'email'
-		}
-	}
-});
-
-var FormView = Backbone.View.extend({});
-
-var view = new FormView({model: new FormModel()});
-
-exports.form = {};
+//var check = require('validator').check,
+//	sanitize = require('validator').sanitize;
+//
+//var schema = {
+//	name: { type: 'text', options: ['Male', 'Female']},
+//	age: { type: 'text', validators: ['required', 'int']}
+//
+//};
+//
+//var validators = {
+//	required: function(
+//}
