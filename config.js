f74945acad21c27fb97c93f43645d231e2aaf65a
Alex Reardon <alexreardon@gmail.com>
2013-08-02 17:56:46 +1000

    defensive config

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
-module.exports = {
+var format = require('util').format,
+	_ = require('underscore');
+
+
+var config = {
 
 	//database
 	db_connection: process.env.fullon_db_connection || 'mongodb://127.0.0.1:27017/test',
@@ -16,3 +20,18 @@ module.exports = {
 	cookie_secret: process.env.fullon_cookie_secret || 'some secret key'
 
 };
+
+var errors = [];
+_.each(config, function(item, i) {
+	if(!item) {
+		errors.push(i);
+	}
+});
+
+if(errors.length) {
+	//crash application
+	throw new Error(format('missing config entries for: %j', errors));
+}
+
+
+module.exports = config;
