afd2e650bd0cfe2e0401b81ad622896ae72519db
Alex Reardon <alexreardon@gmail.com>
2013-08-06 08:48:17 +1000

    db will now throw exception on failure

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 var config = require('./config'),
-	MongoClient = require('mongodb').MongoClient;
+	MongoClient = require('mongodb').MongoClient,
+	format = require('util').format;
 
 
 function connect(cb) {
 	MongoClient.connect(config.db_connection, function(err, db) {
 		if(err){
-			console.error('error connecting to db [%s]: %j', config.db_connection, err);
-			return;
+			//console.error('error connecting to db [%s]: %j', config.db_connection, err);
+			throw new Error(format('error connecting to db [%s]: %j', config.db_connection, err));
 		}
 		cb(db);
 
