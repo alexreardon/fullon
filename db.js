82664e0624e267927ac315a4d4586980f1216d83
Alex Reardon <alexreardon@gmail.com>
2013-08-06 10:28:31 +1000

    db connection failure now ends flow

--- db.js
+++ db.js
@@ -7,6 +7,7 @@ function connect(cb) {
 	MongoClient.connect(config.db_connection, function(err, db) {
 		if(err){
 			cb(format('error connecting to db [%s]: %j', config.db_connection, err));
+			return;
 		}
 		cb(null, db);
 	});
