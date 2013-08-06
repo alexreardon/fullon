689e66d35097b9678773c3d1a55e9e2528c38761
Alex Reardon <alexreardon@gmail.com>
2013-08-06 17:27:20 +1000

    adding error support for demo

--- routes/demo.js
+++ routes/demo.js
@@ -2,11 +2,18 @@ var person = require('../models/person'),
 	config = require('../config');
 
 
-exports.leaderboard = function(req, res){
+exports.leaderboard = function(req, res, next){
 
 	//(query, cb, limit, sort)
 	//find all people with sold > 0, limit: 10, in decending order of amount sold
 	person.find({sold: {$gt: 0}}, function(err, data){
+
+		if(err){
+			next(new Error(err));
+			return;
+		}
+
+
 		res.render('demo', {people: data, leaderboard_size: config.leaderboard_size});
 	}, config.leaderboard_size, {sold: -1});
 
