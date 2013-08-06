e498c19dade10da3987366bc2fa4d0b071614144
Alex Reardon <alexreardon@gmail.com>
2013-08-06 16:32:38 +1000

    fixing demo link

--- routes/demo.js
+++ routes/demo.js
@@ -7,7 +7,7 @@ exports.leaderboard = function(req, res){
 	//(query, cb, limit, sort)
 	//find all people with sold > 0, limit: 10, in decending order of amount sold
 
-	person.find({sold: {$gt: 0}}, function(data){
+	person.find({sold: {$gt: 0}}, function(err, data){
 		res.render('demo', {people: data, leaderboard_size: config.leaderboard_size});
 	}, config.leaderboard_size, {sold: -1});
 
