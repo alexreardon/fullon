c330f925869ad2c940e0d52851bbf7dc5bb24b67
Alex Reardon <alexreardon@gmail.com>
2013-10-07 10:30:45 +1100

    min-height for all sections

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,55 @@
 //namespaces
-window.fullon = {};
-window.fullon.Views = {};
+window.FullOn = {};
+window.FullOn.Views = {};
+
+window.FullOn.Views.CommonView = Backbone.View.extend({
+
+	initialize: function () {
+		this.$sections = $('section');
+		this.$footer = $('footer');
+		this.$nav = $('nav');
+
+		var throttled_resize = _.throttle(function () {
+			this.resize();
+		}.bind(this), 100);
+
+		this.resize();
+
+		$(window).on('resize', function () {
+			throttled_resize();
+		}.bind(this));
+	},
+
+	events: {
+
+	},
+
+	resize: function () {
+		console.log('window resize - common');
+
+//		this.$sections.each(function($section){
+//			console.log('section!');
+//			$section.css('min-height', function(){
+//				this.getMinHeight();
+//			}.bind(this));
+//		}.bind(this));
+
+		var self = this;
+		this.$sections.each(function(){
+			$(this).css('min-height', self.getMinHeight());
+		});
+
+//		_.each(this.$sections, function(section){
+//			$(section).css('min-height', function(){
+//				this.getMinHeight();
+//			}.bind(this));
+//		}.bind(this));
+	},
+
+	getMinHeight: function () {
+		return ($(window).height() - this.$nav.height() - this.$footer.height());
+	}
+
+});
+
+var commonView = new window.FullOn.Views.CommonView();
--- public/js/pages/index.js
+++ public/js/pages/index.js
@@ -1,23 +1,30 @@
 var IndexView = Backbone.View.extend({
 
-	initialize: function () {
-		this.$landing = $('#landing');
-		this.resize();
-		$(window).on('resize', function(){
+		initialize: function () {
+			this.$landing = $('#landing');
+
+			var throttled_resize = _.throttle(function () {
+				this.resize();
+			}.bind(this), 100);
+
 			this.resize();
-		}.bind(this));
-	},
 
-	events: {
+			$(window).on('resize', function () {
+				throttled_resize();
+			}.bind(this));
+		},
 
-	},
+		events: {
 
-	resize: function () {
-		this.$landing.css('min-height', $(window).height());
-	}
+		},
 
+		resize: function () {
+			console.log('window resize - landing');
+			this.$landing.css('min-height', $(window).height());
+		}
 
-});
+	})
+	;
 
 var index = new IndexView();
 
